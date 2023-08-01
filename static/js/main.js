9c099e67ac2fcd0e9bc73b46e834c5d6c399316f
Pramod <single.core.developers@gmail.com>
2023-08-01 11:07:06 +0530

    new

--- static/js/main.js
+++ static/js/main.js
@@ -62,14 +62,14 @@
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100000) {
-            $('.back-to-top').fadeIn('fast');
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('fast');
+            $('.back-to-top').fadeOut('slow');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 15000, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
